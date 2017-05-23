2d64b0690d243a00139154f4edb22f1d2f6f528c
Andy Eccleston <eandy5000@yahoo.com>
2017-05-22 22:06:48 -0500

    added countdown-timer component

---
+++ src/components/bits/CountdownTimer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        {{count}}
+        <button v-if="count === 0" @click="restart">Start</button>
+        <button v-else v-on:click="countDown">Start</button>
+    </div>
+</template>
+
+<script>
+    export default {
+      data () {
+        return {
+          countStart: 5,
+          count: 5
+        }
+      },
+      methods: {
+        countDown () {
+          const setInt = setInterval(() => {
+            this.count--
+            if (this.count === 0) {
+              clearInterval(setInt)
+            }
+          }, 1000)
+        },
+        restart () {
+          this.count = this.countStart
+          this.countDown()
+        }
+      }
+    }
+</script>
+
+<style>
+    
+</style>
